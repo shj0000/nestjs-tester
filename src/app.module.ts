85f529ff913d35540dd9a25a7e1cd18dc8150ff0
shj0000 <shj0000@ex-it.com>
2022-10-22 22:24:08 +0900

    Edit for TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacebookController } from './facebook/facebook.controller';
 import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'test',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),],
   controllers: [AppController, FacebookController, TestController],
   providers: [AppService],
 })
