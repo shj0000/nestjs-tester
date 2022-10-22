e40d3e78d0394df3610722da37294e4e721fef74
shj0000 <shj0000@ex-it.com>
2022-10-22 20:39:21 +0900

    Controller 추가 - facebook, test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FacebookController } from './facebook/facebook.controller';
+import { TestController } from './test/test.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, FacebookController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
