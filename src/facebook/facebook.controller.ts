331f16ef4c5f52027b2f4689b2bad431976a4979
shj0000 <shj0000@ex-it.com>
2022-10-22 21:52:39 +0900

    Edit controller for test

--- src/facebook/facebook.controller.ts
+++ src/facebook/facebook.controller.ts
@@ -2,10 +2,21 @@ import { Controller, Get, Res } from '@nestjs/common';
 
 @Controller('facebook')
 export class FacebookController {
+  
   @Get()
   findAll(@Res() res) {
     // const users = this.usersService.findAll()
+    console.log(res)
 
-    return res.status(200).send(`users`);
+    const testMap = {
+      ct: 1,
+      ct2: 3,
+    }
+
+    return res
+      .status(200)
+      .send(testMap)
+
   }
+
 }
